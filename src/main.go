3eaaad5a170888facbb201b4aaa00d39cbf6601c
Hanasou <royzhang42@gmail.com>
2021-06-22 23:56:19 -0700

    Added everything to references

--- src/main.go
+++ src/main.go
@@ -1,25 +1,4 @@
 package main
 
-import (
-	"fmt"
-)
-
-func structEmbedding() {
-	fmt.Println("Hello")
-	newAb := &AB{
-		A: &A{
-			Afield: 1,
-		},
-		B: &B{
-			Bfield: 2,
-		},
-	}
-	// Notice how we don't have to refer to Afield through A, even though technically Afield is part of A, not AB.
-	fmt.Println(newAb.Afield)
-	// Should also apply for methods. We don't have to call getBField() methods through B because of embedding
-	fmt.Println(newAb.getBField())
-}
-
 func main() {
-	channelLooping()
 }
--- src/references/structs.go
+++ src/references/structs.go
@@ -1,4 +1,4 @@
-package main
+package references
 
 type A struct {
 	Afield int
